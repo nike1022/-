fc07d5e4fe0b9927464390ffd6ce52195544a30e
EbiharaKeisuke <NIKE@EbiharaKeisuke-no-MacBook-Pro-9487.local>
2019-05-04 21:58:39 +0900

    add newfile sample1.py

---
+++ sample1.py
@@ -0,0 +1,13 @@
+
+# coding:utf-8
+
+print("こんにちは、Python!")
+
+
+# クラスの定義
+class SubClass(object):
+    def __init__(self, name):
+        self.name = arg
+
+    def hello(self):
+        print("こんにちは、"+self.name)
